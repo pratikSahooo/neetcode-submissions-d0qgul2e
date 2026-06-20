17f7538407c86a2738992dd4526b0102c5f36c50
pratikSahooo <pratiksahoo710@gmail.com>
2026-06-21 00:25:49 +0530

    Add: check-completeness-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/check-completeness-of-a-binary-tree/submission-1.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        int n = count(root);
+        return dfs(root,n,0);
+    }
+
+    public int count(TreeNode root) {
+        if(root == null) return 0 ;
+        return  1+count(root.left)+count(root.right) ;
+    }
+
+    public boolean dfs(TreeNode root,int n,int indexcount){
+        if(root == null) return true ;
+
+        if(indexcount >= n) return false ;
+
+        return dfs(root,n,2*indexcount+1) && dfs(root,n,2*indexcount+2);
+    } 
+}
