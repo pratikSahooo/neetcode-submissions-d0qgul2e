dee8917fd5642b03eff2970e4868283792cf97c0
pratikSahooo <pratiksahoo710@gmail.com>
2026-07-12 00:10:34 +0530

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+       int []arr = new int[k] ;
+        Map<Integer,Integer> store = new HashMap<Integer,Integer>() ;
+        for(int trav : nums){
+            store.put(trav,store.getOrDefault(trav,0)+1);
+        }
+
+        // for (int i=0;i<k;i++){
+        //     int maxfreq = 0;
+        //     int maxkey = 0 ;
+        //     for(Map.Entry<Integer,Integer> itr : store.entrySet()){
+        //         if (itr.getValue()>maxfreq){
+        //             maxfreq = itr.getValue();
+        //             maxkey =  itr.getKey() ;
+        //         }
+        //     }
+        //     arr[i] = maxkey ;
+        //     store.remove(maxkey) ;
+
+        // }
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for(Map.Entry<Integer,Integer> itr : store.entrySet()){
+                heap.offer(new int[]{itr.getKey(),itr.getValue()});
+        }
+        for (int i=0;i<k;i++){
+            int [] maxkey = heap.poll() ;
+            arr[i] = maxkey[0] ;
+        }
+        return arr ;
+    }
+}
