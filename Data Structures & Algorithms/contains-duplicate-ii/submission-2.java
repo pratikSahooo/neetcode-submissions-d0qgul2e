d437a28ffdc0f5d0e033333e2e0b793a82c7817a
pratikSahooo <pratiksahoo710@gmail.com>
2026-08-07 20:14:31 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // If current number already exists
+            // within the previous k elements
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+
+            // Keep only k elements in the window
+            // if we are not increasing the window size no need to substract the highest value 
+            // after deducton will be the same as window size
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
