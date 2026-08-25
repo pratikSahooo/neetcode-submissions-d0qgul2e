158990ac8548fa8ef5ce2209f51b3524c29c0818
pratikSahooo <pratiksahoo710@gmail.com>
2026-08-25 23:02:00 +0530

    Add: reverse-linked-list-ii - submission-11

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-11.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right){
+            return head;
+        }
+        ListNode leftNode= head;
+        ListNode dummy = new ListNode(0,head);
+        ListNode beforNext = dummy;
+        for(int i = 1; i<left;i++){
+            beforNext = leftNode;
+            leftNode = leftNode.next;
+        }
+
+        ListNode rightNode = leftNode;
+
+        for(int i = left; i<right;i++){
+            rightNode = rightNode.next;
+        }
+        
+        ListNode backup = rightNode.next;
+        ListNode prev = backup;
+        ListNode trav = leftNode ;
+        while(trav!=backup){
+            ListNode temp = trav.next;
+            trav.next = prev;
+            prev = trav;
+            trav = temp;
+        }
+        beforNext.next = rightNode;
+        return dummy.next;
+
+    }
+}
